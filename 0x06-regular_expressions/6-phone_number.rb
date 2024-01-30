17b88745aa400880d374b4d9bf11d612df2fc169
Siskosee <seeskosee@seeskoseesko.com>
2024-01-30 07:37:09 -0800

    10-digit number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_phone_number(input_string)
-  # Use the regular expression /^\d{10}$/ to match a 10-digit phone number
-  matches = input_string.scan(/^\d{10}$/)
-  
-  # Print the match results
-  puts matches.join("\n")
-end
-
-# Accept one argument from the command line
-input_string = ARGV[0]
-
-# Call the matching method with the provided argument
-match_phone_number(input_string)
-
+puts ARGV[0].scan(/^\d{10}$/).join("$\n")
