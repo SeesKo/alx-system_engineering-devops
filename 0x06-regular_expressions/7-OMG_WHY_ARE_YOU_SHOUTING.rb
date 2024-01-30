484f22ec19772c0f3779bfc946c19f373eefcf89
Siskosee <seeskosee@seeskoseesko.com>
2024-01-30 07:55:14 -0800

    capital letters

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_capital_letters(input_string)
-  # Use the regular expression /[A-Z]/ to match capital letters
-  matches = input_string.scan(/[A-Z]/)
-  
-  # Print the match results joined as a string
-  puts matches.join
-end
-
-# Accept one argument from the command line
-input_string = ARGV[0]
-
-# Call the matching method with the provided argument
-match_capital_letters(input_string)
+puts ARGV[0].scan(/[A-Z]/).join
