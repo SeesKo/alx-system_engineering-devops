096bdc9fa315e1434909aacab658b430832e1521
Siskosee <seeskosee@seeskoseesko.com>
2024-01-30 07:58:10 -0800

    matching school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0]
-
-# Using the regular expression /School/ to scan all occurrences
-matches = input_string.scan(/School/)
-
-# Printing the concatenated result or an empty string if no match
-puts matches.empty? ? '' : matches.join
+puts ARGV[0].scan(/School/).join
