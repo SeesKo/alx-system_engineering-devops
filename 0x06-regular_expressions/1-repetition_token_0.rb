6c51c08630da8ec902a6caf88a6ddbfa3a8174a1
Siskosee <seeskosee@seeskoseesko.com>
2024-01-30 03:41:13 -0800

    repetition token

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 
+def match_repetition_token(input_string)
+  # Use the regular expression /hbt*n/ to match the test strings
+  matches = input_string.scan(/hbt*n/)
+  
+  # Print the match results
+  puts matches.join("\n")
+end
+
+# Accept one argument from the command line
 input_string = ARGV[0]
 
-# Using the regular expression /hbt*n/ to match the test strings
-matches = input_string.scan(/hbt*n/)
-
-# Printing the match results
-puts matches.join("\n")
+# Call the matching method with the provided argument
+match_repetition_token(input_string)
