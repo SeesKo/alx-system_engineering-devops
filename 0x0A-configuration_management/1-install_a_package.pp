fa8f89ee389f0ffe7f5d192d8482fcaa7665502b
Siskosee <seeskosee@seeskoseesko.com>
2024-03-22 05:40:29 -0700

    installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/pup
 # Puppet manifest to install Flask package
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
