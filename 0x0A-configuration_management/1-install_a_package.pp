d3848bdbd9e47b9bcf18fed6b021e4a6a6e866e1
Siskosee <seeskosee@seeskoseesko.com>
2024-03-22 12:07:39 -0700

    installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Puppet manifest to install Flask package
+# Puppet manifest to install Flask and Werkzeug packages
 
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
