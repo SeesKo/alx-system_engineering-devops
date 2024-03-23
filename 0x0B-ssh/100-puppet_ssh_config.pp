3595ffd35af138a2cf728169ea726612cfd88bbf
Siskosee <seeskosee@seeskoseesko.com>
2024-03-23 07:03:15 -0700

    use puppet to make changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # Puppet manifest file to configure SSH client
 
-file_line { 'configuration_file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
 }
 
-file_line { 'no_password':
+file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
 }
