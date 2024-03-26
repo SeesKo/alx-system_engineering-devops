beb3fa13b71540624d950523364afd8b4dd36379
Siskosee <seeskosee@seeskoseesko.com>
2024-03-26 16:58:05 -0700

    configure nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,30 @@
 # Puppet manifest to install and configure Nginx server
 
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
+# Update system package lists
+exec { 'Update packages':
+    command => '/usr/bin/apt-get update',
 }
 
+# Install Nginx package
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+    ensure  => 'installed',
+    require => exec['update system'],
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+# Set up custom index page
+file { '/var/www/html/index.html':
+    content => 'Hello World!',
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Configure 301 redirect for /redirect_me
+exec { '301 redirect':
+    command  => '/bin/sed -i "/^server {/a \\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    provider => 'shell',
+    require  => Package['nginx'],
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+# Ensure Nginx service is running
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
 }
