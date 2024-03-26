4d8ab96bf99e2189a6b2dca655d7c36b7bf3b417
Siskosee <seeskosee@seeskoseesko.com>
2024-03-26 16:37:37 -0700

    configure nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,24 @@
 # Puppet manifest to install and configure Nginx server
 
-# Install Nginx package
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Set up custom index page
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Set up custom 404 page
-file { '/var/www/html/custom_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page\n",
-  require => Package['nginx'],
-}
-
-# Enable site configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
-}
-
-# Redirect /redirect_me with a 301 status
-nginx::resource::location { 'redirect_me':
-  ensure   => present,
-  location => '/redirect_me',
-  www_root => '/var/www/html',
-  file     => 'redirect_me',
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
