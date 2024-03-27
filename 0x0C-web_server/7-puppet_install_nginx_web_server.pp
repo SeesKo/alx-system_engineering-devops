5139ee73e6a9036405b4a2ae4adf01250933470e
Siskosee <seeskosee@seeskoseesko.com>
2024-03-26 17:16:28 -0700

    configure nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,14 @@
 # Puppet manifest to install and configure Nginx server
 
 # Update system package lists
-exec { 'update system':
+exec { 'Update packages':
     command => '/usr/bin/apt-get update',
 }
 
 # Install Nginx package
-package { 'nginx':
+package { 'NGINX':
     ensure => 'installed',
-    require => Exec['update system'],
+    require => exec['update system'],
 }
 
 # Set up custom index page
@@ -17,14 +17,14 @@ file { '/var/www/html/index.html':
 }
 
 # Configure 301 redirect for /redirect_me
-exec { 'redirect_me':
+exec { '301 redirect':
     command  => '/bin/sed -i "/^server {/a \    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
     provider => 'shell',
     require  => Package['nginx'],
 }
 
 # Ensure Nginx service is running
-service { 'nginx':
+service { 'NGINX':
     ensure => running,
     require => Package['nginx'],
 }
