831afd563e80ba52f53ad2c859b98b6097dfa825
Siskosee <seeskosee@seeskoseesko.com>
2024-03-30 13:16:54 -0700

    automate custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,30 @@
-# Puppet manifest to automate the task of creating a custom HTTP header response
+# Puppet manifest to install and configure Nginx server
 
-exec { 'update':
+# Update packages
+exec { 'Update packages':
   command => 'sudo apt-get update',
   path    => '/usr/bin/'
 }
+
+# Install Nginx package
 package { 'nginx':
   ensure   => 'installed',
   name     => 'nginx',
   provider => 'apt',
-  require  => Exec['update']
+  require  => Exec['Update packages']
 }
-file_line { 'add_custom_header':
+
+# Add custom header to Nginx configuration
+file_line { 'Custom header':
   path    => '/etc/nginx/sites-available/default',
   line    => "\tadd_header X-Served-By ${hostname};",
   after   => 'listen 80 default_server;',
   require => Package['nginx']
 }
+
+# Restart Nginx service
 exec { 'restart':
   command => 'sudo service nginx restart',
   path    => '/usr/bin/',
-  require => File_line['add_custom_header']
+  require => File_line['Custom header']
 }
