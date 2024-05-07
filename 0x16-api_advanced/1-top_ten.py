360149b0d59b4334399b0efad91a0a90eed8314d
SeesKo <seeskosee@seeskome.com>
2024-05-07 12:26:11 -0700

    print first ten post titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and prints the titles of the first 10
-hot posts listed for a given subreddit"""
-
-import json
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Function to prints the titles of the first 10 hot posts of a
-    subreddit"""
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    url = "https://www.reddit.com/r/"
-    headers = {"User-Agent": "User Agent"}
-    response = requests.get(url + subreddit + "/hot.json?limit=10",
-                            headers=headers, allow_redirects=False)
-    # print(response)
-    if response.status_code == 200:
-        for i in response.json().get("data").get("children"):
-            print(i.get("data").get("title"))
-    else:
-        print("None")
+
+if __name__ == "__main__":
+    top_ten(argv[1])
