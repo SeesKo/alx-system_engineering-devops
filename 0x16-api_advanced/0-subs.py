991170ef205cc21a0f4de7198e161ff59c8ed1df
SeesKo <seeskosee@seeskome.com>
2024-05-07 03:28:30 -0700

    return number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-Script to query the Reddit API and return the
-number of subscribers for a given subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "CustomUserAgent/1.0"}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if "data" in response:
-        return (response.get("data").get("subscribers"))
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        return (0)
+        return 0
