a9b67edef31e7be8ec57ca44d167fbc63ff4b19d
SeesKo <seeskosee@seeskome.com>
2024-05-07 02:12:54 -0700

    return number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,16 +14,12 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyRedditClient/0.1'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-        # Check for success status code
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data.get("data", {}).get("subscribers", 0)
-            return subscribers
-        else:
-            return 0
-    except requests.RequestException:
-        # If there's any error with the request, return 0
+    # Check for success status code
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
+        return subscribers
+    else:
         return 0
